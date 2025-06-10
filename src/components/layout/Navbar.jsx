e4bb74968a24542e54016dd8c0f08fc44434d855
Mateus Noleto <mateusnoletods@gmail.com>
2025-06-09 21:32:55 -0300

    criado navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const StyleNavbar = styled.div`
+ background-color: ${props => props.theme.white};
+ height: 80px;
+ display: flex;
+ align-items: center;
+ padding:0 100px;
+`
+
+const StyleLogo = styled.span`
+    flex: 1;
+    font-weight: bold;
+    font-size: 20px;
+`
+
+function Navbar (){
+    return (
+        <StyleNavbar>
+            <StyleLogo># Seja Dev</StyleLogo>
+            <div>
+                <a hfer="#">Desconectar</a>
+            </div>
+        </StyleNavbar>
+    )
+
+}
+export default Navbar
