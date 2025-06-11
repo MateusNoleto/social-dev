6dbedbbf3572d1b95ac6aefd5584a5e51ca46490
Mateus Noleto <mateusnoletods@gmail.com>
2025-06-11 20:24:40 -0300

    criado componentes de post

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,32 @@ import styled from "styled-components"
 import Navbar from "../src/components/layout/Navbar"
 import Container from "../src/components/layout/Container"
 import CreatePost from "../src/components/cards/CreatePost"
+import H3 from "../src/components/typography/H3"
+import Post from "../src/components/cards/Post"
 
 const Content = styled.div`
   margin: 50px 0;
 `
+const LastPostText = styled(H3)`
+  padding: 40px 0;
+`
+const RefreshPost = styled.span`
+  color: ${props => props.theme.primary};
+  font-weigth: bold;
+  cursor: pointer;
+`
+
+const RefreshPostContainer = styled.div`
+  text-align: center;
+`
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px
+`
+
 
 function HomePage () {
   
@@ -16,6 +38,15 @@ function HomePage () {
         <Content>
          <Container>
             <CreatePost />
+            <LastPostText>Últimas postagens</LastPostText>
+            <RefreshPostContainer>
+              <RefreshPost>Carregar novas postagens</RefreshPost>
+            </RefreshPostContainer>
+            <PostContainer>
+              <Post />
+              <Post />
+              <Post />
+            </PostContainer>  
          </Container>
         </Content>
     </>
---
+++ src/components/typography/H3.jsx
@@ -0,0 +1,8 @@
+import style from "styled-components"
+
+const H3 = style.h3`
+    font-size: 26px;
+    font-weight: bold;
+`
+
+export default H3
